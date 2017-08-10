3db976e9586913fe7c8504b34345d8edfb54987e
Anne Kennedy <annekennedy@Annes-MacBook-Pro-2.local>
2017-08-10 12:41:38 -0500

    successfully sending data in post request and getting proper reponse back from database

--- server/routes/translation.router.js
+++ server/routes/translation.router.js
@@ -39,4 +39,32 @@ router.get('/', function(req, res){
   }); // end pool
 }); // end of GET
 
+router.post('/', function(req, res) {
+  var postedBodyPart = req.body;
+  console.log('here is the return I want:', postedBodyPart.bodypart)
+  console.log(postedBodyPart);
+  pool.connect(function(errorConnectingToDatabase, db, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to the database.');
+      res.sendStatus(500);
+    } else {
+      var queryText = 'SELECT * FROM "translations" WHERE "value" LIKE $1;';
+      db.query(queryText, [postedBodyPart.bodypart], function(errorMakingQuery, result){
+        done();
+        console.log('here is the result I want', result);
+        if(errorMakingQuery) {
+          console.log(errorMakingQuery);
+          console.log('Attempted to query with', queryText);
+          console.log('Error making query');
+          res.sendStatus(500);
+        } else {
+          // console.log(result);
+          // Send back the results
+          res.send(result);
+        }
+      }); // end query
+    } // end if
+  }); // end pool
+});
+
 module.exports = router;
